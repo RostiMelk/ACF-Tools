ef617e22c97a792dc82464eb0632f9dd8411704c
RostiMelk <hello@rosti.no>
2020-04-27 17:26:20 +0200

    2.6.1 - Includes a step by step field code function for google-maps

--- src/scripts/misc.js
+++ src/scripts/misc.js
@@ -36,39 +36,103 @@ function copyCodeToClipboard(fieldCode, subFields) {
 	}
 }
 
-function fieldError() {
-	alert(chrome.i18n.getMessage('fieldError'));
-	throw new Error(chrome.i18n.getMessage('fieldError'));
+function copyModalCode() {
+	$("body").on('click', '#acftoolsCodeModal pre code', function() {
+		code = $(this).text();
+		copyCodeToClipboard(code, subFields = false);
+	});
+	var str = chrome.i18n.getMessage('clickToCopyCode')
+	$('#acftoolsCodeModal').find('pre').append('<span class="copy-code-info">'+str+'</span>');
 }
 
-function codeModal(openModal) {
-	var modal = $('<div id="acftoolsCodeModal"></div>')
+function localizeModal() {
+    //Localize by replacing __MSG_***__ meta tags
+    var objects = $('#acftoolsCodeModal');
+    for (var j = 0; j < objects.length; j++) {
+        var obj = objects[j];
+
+        var valStrH = obj.innerHTML.toString();
+        var valNewH = valStrH.replace(/__MSG_(\w+)__/g, function(match, v1) {
+            return v1 ? chrome.i18n.getMessage(v1) : "";
+        });
+
+        if(valNewH != valStrH) {
+            obj.innerHTML = valNewH;
+        }
+    }
+}
+
+function codeModal(openModal, fieldName, seniority, place) {
+	var modal = $('<div id="acftoolsCodeModal"></div>'),
+		modalInner = $('<div class="acftools-modal-inner"></div>'),
+		modalClose = $('<a href="#" id="closeModal"><span class="dashicons dashicons-no"></span></a>');
 
 	if(openModal == false) {
+		// Remove existing modal if openModal is false
 		$("#acftoolsCodeModal").remove();
 	} else {
+		// Create modal
 		$('body').append(modal);
+		// Append some general elements to the modal
+		$(modal).append(modalInner);
+		$(modalInner).append(modalClose);
+		
+		// Get modal HTML from /static/
+		$.get(chrome.extension.getURL('/static/'+openModal+'.html'), function(data){
+			$('#acftoolsCodeModal .acftools-modal-inner').append(data);
+		});
+
+		// Import gists to static file
 		setTimeout(function() {
-			modal.addClass('active');
-			document.querySelectorAll('pre code').forEach((block) => {
+			$('#acftoolsCodeModal pre code').each(function() {
+				if (typeof $(this).attr('data-gist') !== 'undefined') {
+					var codeBlock = $(this),
+						gist = codeBlock.attr('data-gist');
+
+					$.get(chrome.extension.getURL('/static/gists-'+openModal+'/'+gist+'.txt'), function(data){
+						// HTML tags should not be output as HTML
+						data = data.replace(/</g, "&lt;");
+						data = data.replace(/>/g, "&gt;");
+						data = data.replace(/REPLACE_WITH_FIELD_NAME/g, fieldName);
+						// Change to sub field if sub field
+						if (seniority == 'sub') {
+							data = data.replace('get_field', 'get_sub_field');
+							data = data.replace('the_field', 'the_sub_field');
+						}
+						// Add options if options page
+						if (place == 'options_page') {
+							var fieldNameRe = new RegExp("'" + fieldName + "'", 'g');
+							data = data.replace(fieldNameRe, "'" + fieldName + "', 'options'");
+						}
+						
+						codeBlock.html(data);
+					});
+				}
+			});
+			// Add a function to copy code
+			copyModalCode();
+		},50);
+
+		// Importing takes some time, so we add another delay
+		setTimeout(function() {
+			// Perform localization
+			localizeModal();
+			// Syntax highlighting to gist
+			document.querySelectorAll('#acftoolsCodeModal pre code').forEach((block) => {
 				hljs.highlightBlock(block);
 			});
-			copyModalCode();
-		},300);
+			// Show modal
+			modal.addClass('active');
+		},100);
 	}
-}
-
-function closeModal() {
+	// Close modal when X is clicked
 	$("body").on('click', '#closeModal', function(e) {
 		e.preventDefault();
 		codeModal(false);
 	})
 }
 
-function copyModalCode() {
-	$("body").on('click', '#acftoolsCodeModal pre code', function() {
-		code = $(this).text();
-		copyCodeToClipboard(code, subFields = false);
-	});
-	$('#acftoolsCodeModal').find('pre').append('<span class="copy-code-info">Click to copy code.</span>');
+function fieldError() {
+	alert(chrome.i18n.getMessage('fieldError'));
+	throw new Error(chrome.i18n.getMessage('fieldError'));
 }
